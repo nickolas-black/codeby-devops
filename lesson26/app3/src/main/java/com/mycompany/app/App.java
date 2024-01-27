34e3c0ac5f5d4684b6adad4df2aa9ee690ec9925
Black <gjcjk123@bk.ru>
2024-01-28 02:53:55 +0300

    fix jenkins

--- lesson26/app3/src/main/java/com/mycompany/app/App.java
+++ lesson26/app3/src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello DebOps!";
+    private static final String MESSAGE = "Hello DevOps!";
 
     public App() {}
 
